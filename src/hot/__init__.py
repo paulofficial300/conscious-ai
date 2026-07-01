3aee16b329204a57da6a1f06d8a4ecbb7b02787f
paulofficial300 <pvanderwalker420@gmail.com>
2026-07-01 13:27:38 -0400

    Add HOT module initialization

---
+++ src/hot/__init__.py
@@ -0,0 +1,9 @@
+"""Higher-Order Thought (HOT) module implementation."""
+
+from .hot_module import HigherOrderThought
+from .evaluator import HOTEvaluator
+
+__all__ = [
+    "HigherOrderThought",
+    "HOTEvaluator",
+]
