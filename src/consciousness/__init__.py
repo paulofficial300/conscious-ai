170c21c08c1e226ae7f877b67ae34fce990c3c5d
paulofficial300 <pvanderwalker420@gmail.com>
2026-07-01 13:17:05 -0400

    Add consciousness metrics module initialization

---
+++ src/consciousness/__init__.py
@@ -0,0 +1,9 @@
+"""Consciousness metrics and measurement."""
+
+from .metrics import ConsciousnessMetrics
+from .integration_info import IntegratedInformation
+
+__all__ = [
+    "ConsciousnessMetrics",
+    "IntegratedInformation",
+]
